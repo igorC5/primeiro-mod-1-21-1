5b7b082f05d866996ae8b7218a0b3e2ef80e2fea
igorC5 <igorcandidofs@gmail.com>
2024-10-12 12:25:37 -0300

    first commit

---
+++ src/main/java/net/igor/primeiromod/PrimeiroModClient.java
@@ -0,0 +1,11 @@
+package net.igor.primeiromod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PrimeiroModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/igor/primeiromod/PrimeiroModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.igor.primeiromod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PrimeiroModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
